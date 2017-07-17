0f431ae1128e6d58b9069a3f58708889f32ff991
Alex <driveall@mail.ru>
2017-07-17 04:55:31 +0300

    last commit

---
+++ src/main/java/com/reznik/oauth2/Oauth2Application.java
@@ -0,0 +1,38 @@
+package com.reznik.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+@RestController
+public class Oauth2Application  extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.antMatcher("/**")
+				.authorizeRequests()
+				.antMatchers("/", "/login**")
+				.permitAll()
+				.anyRequest()
+				.authenticated();
+	}
+
+	@RequestMapping("/login")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2Application.class, args);
+	}
+}
